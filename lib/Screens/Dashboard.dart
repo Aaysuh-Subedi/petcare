1682ebc842e2b6fa4efc310322940a2c50b67302
Aasu703 <smellylips508@gmail.com>
2025-11-18 20:49:05 +0545

    Checked the color palates

--- lib/Screens/Dashboard.dart
+++ lib/Screens/Dashboard.dart
@@ -5,6 +5,51 @@ class Dashboard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.green);
+    return Scaffold(
+      backgroundColor: Colors.green,
+      appBar: AppBar(
+        title: Text(
+          'Welcome to Pet Care',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 24,
+          ),
+        ),
+        backgroundColor: Colors.green[900],
+        titleSpacing: 90,
+      ),
+
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            SizedBox(
+              width: 500,
+              height: 50,
+              child: ElevatedButton(
+                onPressed: () {
+                  // Navigate to Pet Profiles Screen
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+                child: Text(
+                  'login Profile',
+                  style: TextStyle(
+                    color: Colors.green,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
