3b42e544279b99d0fdb47b4b2ed540f52612a1ea
Aayush Subedi <smellylips508@gmail.com>
2026-01-30 19:04:42 +0545

    sprint5 commit for developer checkout

--- lib/core/services/storage/token_service.dart
+++ lib/core/services/storage/token_service.dart
@@ -1,30 +1,30 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:petcare/features/auth/presentation/providers/auth_provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'package:petcare/features/auth/presentation/providers/auth_provider.dart';
+// import 'package:shared_preferences/shared_preferences.dart';
 
-// provider
-final tokenServiceProvider = Provider<TokenService>((ref) {
-  return TokenService(prefs: ref.read(sharedPreferencesProvider));
-});
+// // provider
+// final tokenServiceProvider = Provider<TokenService>((ref) {
+//   return TokenService(prefs: ref.read(sharedPreferencesProvider));
+// });
 
-class TokenService {
-  static const String _tokenKey = 'auth_token';
-  final SharedPreferences _prefs;
+// class TokenService {
+//   static const String _tokenKey = 'auth_token';
+//   final SharedPreferences _prefs;
 
-  TokenService({required SharedPreferences prefs}) : _prefs = prefs;
+//   TokenService({required SharedPreferences prefs}) : _prefs = prefs;
 
-  // Save token
-  Future<void> saveToken(String token) async {
-    await _prefs.setString(_tokenKey, token);
-  }
+//   // Save token
+//   Future<void> saveToken(String token) async {
+//     await _prefs.setString(_tokenKey, token);
+//   }
 
-  // Get token
-  Future<String?> getToken() async {
-    return _prefs.getString(_tokenKey);
-  }
+//   // Get token
+//   Future<String?> getToken() async {
+//     return _prefs.getString(_tokenKey);
+//   }
 
-  // Remove token (for logout)
-  Future<void> removeToken() async {
-    await _prefs.remove(_tokenKey);
-  }
-}
+//   // Remove token (for logout)
+//   Future<void> removeToken() async {
+//     await _prefs.remove(_tokenKey);
+//   }
+// }
